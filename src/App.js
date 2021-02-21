2dcef2462d7aa5bb330a0736b767fb26df3a7fd8
Developer-felix <onjombafelix1@gmail.com>
2021-02-21 02:47:10 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Pizaaaa</h1>
+     <Navbar/>
+      
+    </div>
+  );
+}
+
+export default App;
